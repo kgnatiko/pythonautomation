4800c5e4f6cac08fa7a96e4670fa3a82c0a9a56d
Komi <kgnatiko@gmail.com>
2019-10-16 10:10:26 -0400

    Clean up and refactor

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-""" Webotron: Deploy websitstes with AWS.
+"""Webotron: Deploy websitstes with AWS.
 
 Webotron automates the process of deploying static websites to AWS.
 - Configure aws s3 Buckets
@@ -11,15 +11,14 @@ Webotron automates the process of deploying static websites to AWS.
 - Configure DNS with AWS Route 53
 - Configure a content delivery network and SSL with AWS Cloudfront
 """
-from pathlib import Path
-import mimetypes
+
 import boto3
 import click
-
+from bucket import BucketManager
 
 
 session = boto3.Session(profile_name='python')
-s3 = session.resource('s3')
+bucket_manager = BucketManager(session)
 
 
 @click.group()
@@ -29,19 +28,17 @@ def cli():
 
 
 @cli.command('list-bucket-object')
-
-
 @click.argument('bucket')
 def list_bucket_object(bucket):
     """List Objects in an S3 bucket."""
-    for obj in s3.Bucket(bucket).objects.all():
+    for obj in bucket_manager.all_objects(bucket):
         print(obj)
 
 
 @cli.command('list-buckets')
 def list_buckets():
     """List all s# buckets."""
-    for bucket in s3.buckets.all():
+    for bucket in bucket_manager.all_buckets():
         print(bucket)
 
 
@@ -49,33 +46,9 @@ def list_buckets():
 @click.argument('bucket')
 def setup_bucket(bucket):
     """Create and configure S3 bucket."""
-    s3_bucket = s3.create_bucket(Bucket=bucket)
-    policy = """
-    {
-        "Version":"2012-10-17",
-        "Statement":[{
-        "Sid":"PublicReadGetObject",
-        "Effect":"Allow",
-        "Principal": "*",
-        "Action":["s3:GetObject"],
-        "Resource":["arn:aws:s3:::%s/*"
-          ]
-        }
-        ]
-    }
-    """ % s3_bucket.name
-    policy = policy.strip()
-    pol = s3_bucket.Policy()
-    pol.put(Policy=policy)
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={
-        'ErrorDocument': {
-            'Key': 'error.html'
-        },
-        'IndexDocument': {
-            'Suffix': 'index.html'
-        }
-    })
+    s3_bucket = bucket_manager.init_bucket(bucket)
+    bucket_manager.set_policy(s3_bucket)
+    bucket_manager.configure_website(s3_bucket)
 
 
 def upload_files(s3_bucket, path, key):
@@ -94,16 +67,7 @@ def upload_files(s3_bucket, path, key):
 @click.argument('bucket')
 def sync(pathname, bucket):
     """Sync content of PATHNAME to Bucket."""
-    s3_bucket = s3.Bucket(bucket)
-    root = Path(pathname).expanduser().resolve()
-
-    def handle_directory(target):
-        for p in target.iterdir():
-            if p.is_dir():
-                handle_directory(p)
-            if p.is_file():
-                upload_files(s3_bucket, str(p), str(p.relative_to(root)))
-    handle_directory(root)
+    bucket_manager.sync(pathname, bucket)
 
 
 if __name__ == '__main__':
